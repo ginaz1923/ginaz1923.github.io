be3308bbd7e9497db9f8fc6c641e34a8e5fcd5c0
ginaz1923 <91861685+ginaz1923@users.noreply.github.com>
2021-11-09 15:10:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Gina Zhao"; // update the part BETWEEN the "quotes"
+let userName = "ginaz1923"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
