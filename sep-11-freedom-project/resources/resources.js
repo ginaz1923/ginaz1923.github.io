843aa9509a4891b7a77f51609fda4e341a1f77e7
Gina Zhao <ginaz1923@hstat.org>
2023-05-21 21:17:35 -0400

    hlp

---
+++ sep-11-freedom-project/resources/resources.js
@@ -0,0 +1,9 @@
+var resources = [
+    //amiya
+    { name : "amiya",  type:"png", src:"characters/amiya/guardmiya_idle.png" },
+
+    //bgm
+    {name: "chernobog.mp3." type:"js", src: "bgm/chernobog/mp3"}
+]
+
+export default resources;
